7d2d8503e19dc7e9b2c182e4eb5210dd11bfd71b
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2024-02-18 20:12:27 +1100

    changes

---
+++ packages/interactions-shared/src/index.js
@@ -0,0 +1,8 @@
+// Relatives
+import utility from './utility';
+import InteractionsContext from './InteractionsContext';
+import InteractionsContextProvider from './InteractionsContextProvider';
+import InteractionsHelper from './InteractionsHelper';
+import useInteractions from './hooks/useInteractions';
+
+export { utility, InteractionsContext, InteractionsContextProvider, InteractionsHelper, useInteractions };
