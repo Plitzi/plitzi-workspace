bb0cd74a98a07782e686d8a4ecac41ab6a2f390b
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2024-05-11 19:55:12 +1000

    changes

--- apps/builder/src/modules/App/AppContainer/AppContainer.js
+++ apps/builder/src/modules/App/AppContainer/AppContainer.js
@@ -42,7 +42,7 @@ const AppContainer = props => {
   return (
     <div className="flex flex-col grow overflow-auto">
       <AppHeader setTabSelected={setTabSelected} />
-      <div className="flex relative basis-0 grow bg-blue-100">
+      <div className="flex relative basis-0 grow bg-blue-100 max-w-[100vw]">
         {!previewMode && <AppSidebar onSelect={setTabSelected} selected={tabSelected} />}
         {!previewMode && tabSelected && !['marketplace', 'integrations', 'settings'].includes(tabSelected) && (
           <div className="flex h-full bg-white">
