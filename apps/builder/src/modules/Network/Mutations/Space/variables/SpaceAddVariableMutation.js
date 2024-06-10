99619248f55a02967c4d3f27e8735f5f37c13ea4
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2024-06-10 14:45:33 +1000

    changes

--- apps/builder/src/modules/Network/Mutations/Space/variables/SpaceAddVariableMutation.js
+++ apps/builder/src/modules/Network/Mutations/Space/variables/SpaceAddVariableMutation.js
@@ -2,7 +2,7 @@
 import { gql } from '@apollo/client/core';
 
 const SpaceAddVariableMutation = gql`
-  mutation SpaceAddVariableMutation($environment: String!, $name: String!, $description: String, category: String, type: String!, value: String, when: String) {
+  mutation SpaceAddVariableMutation($environment: String!, $name: String!, $description: String, $category: String, $type: String!, $value: String, $when: String) {
     SpaceAddVariable(environment: $environment, name: $name, description: $description, category: $category, type: $type, value: $value, when: $when) {
       name
       description
