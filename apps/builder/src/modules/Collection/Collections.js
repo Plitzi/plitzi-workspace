1d400a385529958ea7bd1098bb41c59c6bafc32e
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-01-19 18:56:38 +1100

    changes in collections

--- apps/builder/src/modules/Collection/Collections.js
+++ apps/builder/src/modules/Collection/Collections.js
@@ -1,8 +1,9 @@
 // Packages
-import React, { use } from 'react';
-import noop from 'lodash/noop';
+import React, { use, useCallback } from 'react';
 import classNames from 'classnames';
-import Button from '@plitzi/plitzi-ui-components/Button';
+import Button from '@plitzi/plitzi-ui/Button';
+import Flex from '@plitzi/plitzi-ui/Flex';
+import Icon from '@plitzi/plitzi-ui/Icon';
 import Modal from '@plitzi/plitzi-ui-components/Modal';
 import useModal from '@plitzi/plitzi-ui-components/Modal/useModal';
 
@@ -17,63 +18,59 @@ import CollectionContext from './CollectionContext';
  * @returns {React.ReactElement}
  */
 const Collections = props => {
-  const { collectionId, onSourceChange = noop } = props;
+  const { collectionId, onSourceChange } = props;
   const { showModal } = useModal();
   const { collections, removeCollection } = use(CollectionContext);
 
-  const handleClickAddCollection = () => onSourceChange(undefined);
+  const handleClickAddCollection = useCallback(() => onSourceChange?.(undefined), [onSourceChange]);
 
-  const handleClickRemoveCollection = id => async e => {
-    e.stopPropagation();
-    const response = await showModal(
-      <Modal.Header>
-        <h5>Remove Collection</h5>
-      </Modal.Header>,
-      <Modal.Body>
-        <h4 className="px-3 py-2">Do you want to remove this item ?</h4>
-      </Modal.Body>,
-      null,
-      { placement: 'center', renderFooter: true }
-    );
+  const handleClickRemoveCollection = useCallback(
+    id => async e => {
+      e.stopPropagation();
+      const response = await showModal(
+        <Modal.Header>
+          <h5>Remove Collection</h5>
+        </Modal.Header>,
+        <Modal.Body>
+          <h4 className="px-3 py-2">Do you want to remove this item ?</h4>
+        </Modal.Body>,
+        null,
+        { placement: 'center', renderFooter: true }
+      );
 
-    if (response.result) {
-      removeCollection(id);
-      if (collectionId === id) {
-        onSourceChange();
+      if (response.result) {
+        removeCollection(id);
+        if (collectionId === id) {
+          onSourceChange();
+        }
       }
-    }
-  };
+    },
+    [showModal, removeCollection, onSourceChange]
+  );
 
-  const handleClick = collectionId => () => onSourceChange(collectionId);
+  const handleClick = useCallback(collectionId => () => onSourceChange(collectionId), [onSourceChange]);
 
   return (
-    <div className="flex flex-col">
-      <Button
-        intent="custom"
-        size="custom"
-        onClick={handleClickAddCollection}
-        className="px-4 py-3 bg-gray-600 text-white"
-      >
-        <i className="fas fa-database fa-2x mr-4" />
+    <Flex direction="column" gap={4} className="w-full">
+      <Button iconPlacement="before" size="sm" onClick={handleClickAddCollection}>
+        <Button.Icon icon="fa-solid fa-plus" />
         Add Collection
       </Button>
-      <div className="flex flex-col">
+      <div className="w-full border-solid border-b border-gray-200" />
+      <Flex direction="column" gap={2}>
         {Object.values(collections).map((collection, i) => {
           const { id, namePlural } = collection;
 
           return (
             <div
               key={i}
-              className={classNames(
-                'w-full flex px-3 py-4 items-center justify-between hover:bg-blue-200 border-gray-300 border-b cursor-pointer',
-                {
-                  'bg-blue-200/50': collectionId === id
-                }
-              )}
+              className={classNames('w-full flex items-center justify-between cursor-pointer', {
+                'text-primary-500': collectionId === id
+              })}
               onClick={handleClick(id)}
             >
               <div className="flex items-center">
-                <i className="fas fa-database mr-1 text-blue-400" />
+                <Icon icon="fas fa-database" intent="custom" className="mr-1" />
                 {namePlural}
               </div>
               <div className="flex">
@@ -86,8 +83,8 @@ const Collections = props => {
             </div>
           );
         })}
-      </div>
-    </div>
+      </Flex>
+    </Flex>
   );
 };
 
