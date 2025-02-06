5b7d57146ebf39758c6e7a020bea60caa0a40023
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-02-06 22:57:42 +1100

    bugfixes sdk-event-bridge

--- packages/sdk-event-bridge/src/index.ts
+++ packages/sdk-event-bridge/src/index.ts
@@ -1,13 +1,15 @@
 // Relatives
+import EventBridge from './EventBridge';
 import EventBridgeContext from './EventBridgeContext';
 import EventBridgeContextProvider from './EventBridgeContextProvider';
-import EventBridge from './EventBridge';
+import useEventBridge from './hooks/useEventBridge';
 
 export * from './EventBridge';
 export * from './EventBridgeContext';
 export * from './EventBridgeContextProvider';
 export * from './EventBridgeHelper';
+export * from './hooks/useEventBridge';
 
-export { EventBridge, EventBridgeContext, EventBridgeContextProvider };
+export { EventBridge, EventBridgeContext, EventBridgeContextProvider, useEventBridge };
 
 export default EventBridge;
