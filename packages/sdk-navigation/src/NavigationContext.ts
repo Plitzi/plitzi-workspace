472d7111ff2759f2348014e4a56238ca1d638555
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-03-01 12:26:54 +1100

    types fixed

--- packages/sdk-navigation/src/NavigationContext.ts
+++ packages/sdk-navigation/src/NavigationContext.ts
@@ -1,8 +1,11 @@
 import { createContext } from 'react';
 
-import type { NavigationContextValue } from '@plitzi/sdk-shared';
+import type { NavigationContextValue as NavigationContextValueShared } from '@plitzi/sdk-shared';
+import type { Helmet } from 'react-helmet-async';
 
 export type NavigationStatus = 'normal' | 'redirect' | 'notFound' | 'accessDenied';
+
+export type NavigationContextValue = NavigationContextValueShared<typeof Helmet>;
 
 const navigationContextDefaultValue = {} as NavigationContextValue;
 
--- packages/sdk-shared/src/types/NavigationTypes.ts
+++ packages/sdk-shared/src/types/NavigationTypes.ts
@@ -1,12 +1,12 @@
 export type QueryParams = Record<string, string>;
 export type RouteParams = Record<string, string>;
 
-export type NavigationContextValue = {
+export type NavigationContextValue<T = unknown> = {
+  Helmet?: T;
   navigate: (url: string, isExternal?: boolean) => void;
   urlSearchParams?: Record<string, string>;
   routeParams: RouteParams;
   queryParams: QueryParams;
   hostname?: string;
   currentPageId?: string;
-  Helmet?: unknown;
 };
