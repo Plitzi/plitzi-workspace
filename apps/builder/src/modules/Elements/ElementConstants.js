7eb88466cf153b78fdc08168354a3ceabc2f768d
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-07-28 18:46:24 +0100

    clean up

--- apps/builder/src/modules/Elements/ElementConstants.js
+++
@@ -1,3 +0,0 @@
-export const REFERENCE_TYPE_ELEMENT = 'element';
-export const REFERENCE_TYPE_TEMPLATE = 'template';
-export const REFERENCE_TYPE_SEGMENT = 'segment';
--- apps/builder/src/modules/Elements/index.ts
+++ apps/builder/src/modules/Elements/index.ts
@@ -1,3 +1,5 @@
 import Elements from './Elements';
 
+export * from './Elements';
+
 export default Elements;
