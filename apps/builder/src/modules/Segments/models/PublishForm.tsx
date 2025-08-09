aa61334044e46f51e61db5d54cf39d2fc7a9221a
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-08-09 19:35:04 +0100

    moving files to TS

---
+++ apps/builder/src/modules/Segments/models/PublishForm.tsx
@@ -0,0 +1,52 @@
+import Alert from '@plitzi/plitzi-ui/Alert';
+import Button from '@plitzi/plitzi-ui/Button';
+import Form, { useForm } from '@plitzi/plitzi-ui/Form';
+import { useCallback } from 'react';
+import { z } from 'zod';
+
+const publishFormSchema = z.object({
+  environment: z.enum(['production', 'staging', 'development']),
+  description: z.string()
+});
+
+export type PublishFormProps = {
+  environment?: 'production' | 'staging' | 'development';
+  description?: string;
+  onClose?: () => void;
+  onSubmit?: (values: z.infer<typeof publishFormSchema>) => void;
+};
+
+const PublishForm = ({ environment = 'development', description = '', onClose, onSubmit }: PublishFormProps) => {
+  const form = useForm({ initialValues: { environment, description }, config: { schema: publishFormSchema } });
+
+  const handleSubmitInternal = useCallback(
+    (values: z.infer<typeof publishFormSchema>) => onSubmit?.(values),
+    [onSubmit]
+  );
+
+  return (
+    <Form form={form} onSubmit={handleSubmitInternal} className="gap-4">
+      <Form.Body>
+        <Alert className="mb-4 text-white" intent="info">
+          Make a snapshot and save it into an environment to later publish it
+        </Alert>
+        <Form.Select name="environment" label="Environment" size="xs">
+          <option value="development">Development</option>
+          <option value="staging">Staging</option>
+          <option value="live">Live</option>
+        </Form.Select>
+        <Form.TextArea name="description" label="Description" placeholder="Brief description changes..." size="xs" />
+      </Form.Body>
+      <Form.Footer justify="end">
+        <Button onClick={onClose} size="sm">
+          Cancel
+        </Button>
+        <Button type="submit" size="sm">
+          Snapshot!
+        </Button>
+      </Form.Footer>
+    </Form>
+  );
+};
+
+export default PublishForm;
---
+++ apps/builder/src/modules/Segments/models/SegmentForm.tsx
@@ -0,0 +1,53 @@
+import Button from '@plitzi/plitzi-ui/Button';
+import Form, { useForm } from '@plitzi/plitzi-ui/Form';
+import { useCallback } from 'react';
+import { z } from 'zod';
+
+const segmentFormSchema = z.object({
+  identifier: z.string(),
+  name: z.string().min(2),
+  description: z.string()
+});
+
+export type SegmentFormProps = {
+  className?: string;
+  identifier?: string;
+  name?: string;
+  description?: string;
+  onClose?: () => void;
+  onSubmit?: (values: z.infer<typeof segmentFormSchema>) => void;
+};
+
+const SegmentForm = ({
+  identifier = 'new-segment',
+  name = 'New Segment',
+  description = '',
+  onSubmit,
+  onClose
+}: SegmentFormProps) => {
+  const form = useForm({ initialValues: { name, description, identifier }, config: { schema: segmentFormSchema } });
+
+  const handleSubmitInternal = useCallback(
+    (values: z.infer<typeof segmentFormSchema>) => onSubmit?.(values),
+    [onSubmit]
+  );
+
+  return (
+    <Form form={form} onSubmit={handleSubmitInternal} className="gap-4">
+      <Form.Body>
+        <Form.Input name="name" label="Segment Name" size="xs" />
+        <Form.TextArea name="description" label="Segment Description" size="xs" />
+      </Form.Body>
+      <Form.Footer justify="end">
+        <Button onClick={onClose} size="sm">
+          Cancel
+        </Button>
+        <Button type="submit" size="sm">
+          Submit
+        </Button>
+      </Form.Footer>
+    </Form>
+  );
+};
+
+export default SegmentForm;
