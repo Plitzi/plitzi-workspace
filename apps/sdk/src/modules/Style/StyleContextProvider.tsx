9be8df598435448234fe83ac1e3f9f33b9c8b0a4
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-10-17 00:10:53 +0100

    linter

--- apps/sdk/src/modules/Style/StyleContextProvider.tsx
+++ apps/sdk/src/modules/Style/StyleContextProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useMemo, use } from 'react';
 
 import NetworkInternalContext from '@modules/Network/contexts/NetworkInternalContext';
