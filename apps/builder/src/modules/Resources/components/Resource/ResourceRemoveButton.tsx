50c5cf99ca9993f01e5fdcf0b85630e4380435d5
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-10-29 16:17:19 +0000

    changes

--- apps/builder/src/modules/Resources/components/Resource/ResourceRemoveButton.tsx
+++ apps/builder/src/modules/Resources/components/Resource/ResourceRemoveButton.tsx
@@ -5,8 +5,12 @@ export type ResourceRemoveButtonProps = { onRemove?: (e: MouseEvent) => void };
 const ResourceRemoveButton = ({ onRemove }: ResourceRemoveButtonProps) => {
   return (
     <div className="absolute top-0 left-0 hidden h-full w-full items-center justify-center bg-black/40 group-hover:flex">
-      <div className="flex aspect-square cursor-pointer items-center justify-center rounded border border-gray-300 bg-white px-1">
-        <i className="fas fa-trash-alt text-lg text-red-500 hover:text-red-400" title="Remove" onClick={onRemove} />
+      <div
+        className="flex aspect-square cursor-pointer items-center justify-center rounded border border-gray-300 bg-white px-1"
+        onClick={onRemove}
+        title="Remove"
+      >
+        <i className="fas fa-trash-alt text-lg text-red-500 hover:text-red-400" />
       </div>
     </div>
   );
