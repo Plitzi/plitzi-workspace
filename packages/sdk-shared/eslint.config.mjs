25334d0f88f839aa1ebb7a8338808592e213c6fd
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-11-01 17:09:30 +0000

    regression in eslint with a specific rule

--- packages/sdk-shared/eslint.config.mjs
+++ packages/sdk-shared/eslint.config.mjs
@@ -35,6 +35,9 @@ export default tsEslint.config({
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
+    // Turned off for eslint@9.39.0
+    // @see https://github.com/eslint/eslint/issues/20272
+    '@typescript-eslint/unified-signatures': 'off',
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
