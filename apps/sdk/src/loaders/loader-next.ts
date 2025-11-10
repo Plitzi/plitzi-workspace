568c8d1de4af3c764a95de0c51ad477fccc09ef9
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-11-10 21:58:31 +0000

    bugfix loader

---
+++ apps/sdk/src/loaders/loader-next.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+const isServer = !(process as NodeJS.Process & { browser?: boolean }).browser;
+
+type Sdk = Record<string, unknown> & { default: unknown };
+
+// @ts-ignore
+const sdk: Sdk = isServer ? await import('./ssr/plitzi-sdk.js') : await import('./plitzi-sdk.js');
+
+export const { ...exports } = sdk;
+
+export default sdk.default;
