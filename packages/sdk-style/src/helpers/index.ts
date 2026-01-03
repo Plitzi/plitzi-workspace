0260455552234ec274e88551563568453ea1cc70
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2026-01-03 01:19:03 +0000

    clean up

--- packages/sdk-style/src/helpers/index.ts
+++ packages/sdk-style/src/helpers/index.ts
@@ -1,22 +1,13 @@
-import calculateBindings from './calculateBindings';
 import calculateInheriting from './calculateInheriting';
 import formatCssFromSelector from './formatCssFromSelector';
 import generateStyleSelector from './generateStyleSelector';
 import processCssVariables from './processCssVariables';
 import processSelector from './processSelector';
 
-export * from './calculateBindings';
 export * from './calculateInheriting';
 export * from './formatCssFromSelector';
 export * from './generateStyleSelector';
 export * from './processCssVariables';
 export * from './processSelector';
 
-export {
-  calculateInheriting,
-  processCssVariables,
-  calculateBindings,
-  formatCssFromSelector,
-  generateStyleSelector,
-  processSelector
-};
+export { calculateInheriting, processCssVariables, formatCssFromSelector, generateStyleSelector, processSelector };
