1878abb02bd5eaf3d3b6bbbec22b0392eb08bbe9
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2026-07-05 19:53:03 +0100

    clean up legacy modules

--- apps/builder/src/modules/OpenAI_Deprecated/components/VoiceVisualizer/helpers/index.ts
+++
@@ -1,8 +0,0 @@
-import { drawByBlob } from './drawByBlob';
-import { drawByLiveStream } from './drawByLiveStream';
-import { getBarsData } from './getBarsData';
-import { initialCanvasSetup } from './initialCanvasSetup';
-import { paintLine } from './paintLine';
-import { paintLineFromCenterToRight } from './paintLineFromCenterToRight';
-
-export { paintLine, drawByBlob, initialCanvasSetup, drawByLiveStream, paintLineFromCenterToRight, getBarsData };
--- apps/builder/src/modules/OpenAI_Deprecated/components/VoiceVisualizer/index.ts
+++
@@ -1,5 +0,0 @@
-import VoiceVisualizer from './VoiceVisualizer';
-
-export * from './VoiceVisualizer';
-
-export default VoiceVisualizer;
